ad1ff2b02c2efd7b17031ae1137c4d4de11c1bb9
exploit <hero207@126.com>
2016-03-04 16:43:30 +0800

    Added user and doctor name to video session api

--- app/serializers/video_session_serializer.rb
+++ app/serializers/video_session_serializer.rb
@@ -1,3 +1,11 @@
 class VideoSessionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :symptom, :doctor_id, :start_time, :finish_time, :status
+  attributes :id, :user_id, :symptom, :doctor_id, :start_time, :finish_time, :status, :user_name, :doctor_name
+
+  def user_name
+  	object.user.try(:name)
+	end
+
+	def doctor_name
+		object.doctor.try(:name)
+	end
 end
